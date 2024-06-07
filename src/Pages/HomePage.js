091b6c47beec0a8ec3fafbf89e84b07e992ac24a
Akshxar <shakya.ani47@gmail.com>
2024-06-07 22:48:32 +0530

    ADD THE HEADING

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import FetchMovies from '../Components/MovieList/FetchMovies'
+import FetchMovies from '../Components/MovieList/FetchMovies';
+import './HomePage.css'
 
 const HomePage = () => {
   return (
     <div>
-        <h1>THUNDER OF GOD</h1>
+        <h1 className='h1'>THUNDER OF GOD</h1>
       
         <FetchMovies/>
       
